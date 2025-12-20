e61c1415a1b25512461f4d81987529098616b095
lambdaground <lambdaground@gmail.com>
2025-12-20 17:49:47 +0900

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import app, { setupApp } from "../server/index";
+import app, { setupApp } from "../server/index.js";
 
 export default async function handler(req: any, res: any) {
   // 요청이 들어올 때 DB와 라우터를 연결합니다.
