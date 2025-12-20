d3e267d4ecf0b68958cd25b35f999c5e608e10c9
lambdaground <lambdaground@gmail.com>
2025-12-20 17:05:21 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      // 👇 [중요] 이 줄을 추가해서 @assets가 attached_assets 폴더임을 알려줍니다.
+      "@assets": path.resolve(__dirname, "./attached_assets"),
     },
   },
   build: {
-    outDir: "dist", // 결과물이 나올 폴더 이름
+    outDir: "dist",
   },
 })
