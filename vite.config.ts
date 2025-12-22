270f5b6d91639e2945969a2348e6f96a92d606c0
lambdaground <lambdaground@gmail.com>
2025-12-23 00:38:59 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/lotto/',    // 새로 추가함
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
