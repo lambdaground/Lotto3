dea5332dca247f29a0e6c344ee32f8304cc326b2
lambdaground <lambdaground@gmail.com>
2026-01-03 20:43:54 +0900

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,11 @@
-import app, { setupApp } from "../server/index.js";
+// api/index.ts
+import app, { setupApp } from '../server/index'; 
 
+// Vercel이 실행하는 핸들러 함수
 export default async function handler(req: any, res: any) {
-  // 요청이 들어올 때 DB와 라우터를 연결합니다.
-  await setupApp();
-  // 연결된 앱으로 요청을 처리합니다.
+  // 1. 요청이 들어오면, 가장 먼저 라우트 설정을 완료하기 위해 기다립니다.
+  await setupApp(); 
+  
+  // 2. 설정이 끝난 꽉 찬 app에게 요청을 넘깁니다.
   app(req, res);
 }
